a7abbcfdadd3b8aa171765a9bcf55a4aca4cc729
ashikasurathkal <151603121+ashikasurathkal@users.noreply.github.com>
2023-11-21 17:06:58 +0530

    Add files via upload

---
+++ sim.js
@@ -0,0 +1,17 @@
+let inp1  = document.getElementById('num1');
+let inp2  = document.getElementById('num2');
+let inp3  = document.getElementById('num3');
+
+
+
+
+function si(){
+    inp1=inp1.value;
+    inp2=inp2.value;
+    inp3=inp3.value;
+    let c = inp1* inp2* inp3;
+    document.getElementById('si').innerHTML= c ;
+
+}
+
+
